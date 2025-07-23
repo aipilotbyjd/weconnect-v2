94ba989a71e6e6040f8545c83c12bcde73ca5483
Jaydeep Dhrangiya <recentpagetoday@gmail.com>
2025-07-23 23:29:33 +0530

    Initial commit

---
+++ apps/weconnect-v2/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'weconnect-v2',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/weconnect-v2',
+};
